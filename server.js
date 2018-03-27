815edfc3de23dea6e4daa56bd8d2711c4a1d7419
Tyler Mustard <tjm.0084@gmail.com>
2018-03-27 14:33:16 -0600

    ready

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
-var http = require("http");
-var express = require("express");
-var twilio = require("twilio");
+// var http = require("http");
+// var express = require("express");
+// var twilio = require("twilio");
+//
+// var app = express();
+//
+// app.post("/sms", function(req, res) {
+//   var twilio = require("twilio");
+//   var twiml = new twilio.TwimlResponse();
+//   twiml.message("It worked!");
+//   res.writeHead(200, { "Content-Type": "text/xml" });
+//   res.end(twiml.toString());
+// });
+//
+// http.createServer(app).listen(process.env.PORT || 1337, function() {
+//   console.log("Express server listening on port 1337");
+// });
 
-var app = express();
+const http = require("http");
+const express = require("express");
+const MessagingResponse = require("twilio").twiml.MessagingResponse;
 
-app.post("/sms", function(req, res) {
-  var twilio = require("twilio");
-  var twiml = new twilio.TwimlResponse();
-  twiml.message("The Robots are coming! Head for the hills!");
+const app = express();
+app.post("/sms", (req, res) => {
+  const twiml = new MessagingResponse();
+  twiml.message("HIHIHIH");
   res.writeHead(200, { "Content-Type": "text/xml" });
   res.end(twiml.toString());
 });
