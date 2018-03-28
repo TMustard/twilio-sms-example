9b3b8797e728bbaa012f43ad6c6dc4dd04a5bf43
Tyler Mustard <tjm.0084@gmail.com>
2018-03-28 10:26:14 -0600

    ready

--- server.js
+++ server.js
@@ -1,21 +1,3 @@
-// var http = require("http");
-// var express = require("express");
-// var twilio = require("twilio");
-//
-// var app = express();
-//
-// app.post("/sms", function(req, res) {
-//   var twilio = require("twilio");
-//   var twiml = new twilio.TwimlResponse();
-//   twiml.message("It worked!");
-//   res.writeHead(200, { "Content-Type": "text/xml" });
-//   res.end(twiml.toString());
-// });
-//
-// http.createServer(app).listen(process.env.PORT || 1337, function() {
-//   console.log("Express server listening on port 1337");
-// });
-
 const http = require("http");
 const express = require("express");
 const MessagingResponse = require("twilio").twiml.MessagingResponse;
