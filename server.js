d9256ffca56a27576dfa798789a052b81198db3a
Tyler Mustard <tjm.0084@gmail.com>
2018-03-28 11:18:26 -0600

    ready

--- server.js
+++ server.js
@@ -2,18 +2,45 @@ const http = require("http");
 const express = require("express");
 const MessagingResponse = require("twilio").twiml.MessagingResponse;
 const bodyParser = require("body-parser");
+const morgan = require("morgan");
 const app = express();
+const server = http.createServer(app);
+const port = parseInt(process.env.PORT || 3000);
+const devMode = process.env.NODE_ENV !== "production";
+let msg = "";
 
-app.use(bodyParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan(devMode ? "dev" : "combined"));
 
 app.post("/sms", (req, res) => {
   const twiml = new MessagingResponse();
   twiml.message("Working!");
   res.writeHead(200, { "Content-Type": "text/xml" });
   res.end(twiml.toString());
-  console.log(req.body.Body);
+  msg = req.body.Body;
+  console.log(msg);
 });
 
-http.createServer(app).listen(process.env.PORT || 1337, function() {
-  console.log("Express server listening on port 1337");
-});
+app.use(notFound);
+app.use(errorHandler);
+
+server
+  .listen(port)
+  .on("error", console.error.bind(console))
+  .on("listening", console.log.bind(console, "Listening on " + port));
+
+function notFound(req, res, next) {
+  const url = req.originalUrl;
+  if (!/favicon\.ico$/.test(url) && !/robots\.txt$/.test(url)) {
+    // Don't log less important auto requests
+    console.error("[404: Requested file not found] ", url);
+  }
+  res.status(404).send({ error: "Url not found", status: 404, url });
+}
+
+function errorHandler(err, req, res, next) {
+  console.error("ERROR", err);
+  const stack = devMode ? err.stack : undefined;
+  res.status(500).send({ error: err.message, stack, url: req.originalUrl });
+}
