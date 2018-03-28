14200af9cc997ed716ec1ebf6f43dee85bada414
Tyler Mustard <tjm.0084@gmail.com>
2018-03-28 09:37:21 -0600

    ready

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const MessagingResponse = require("twilio").twiml.MessagingResponse;
 const app = express();
 app.post("/sms", (req, res) => {
   const twiml = new MessagingResponse();
-  twiml.message(req.body);
+  twiml.message("Working!");
   res.writeHead(200, { "Content-Type": "text/xml" });
   res.end(twiml.toString());
 });
